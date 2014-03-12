26649fcc239ff2cd7d62ff4ea730c2b4ba4b899f
Etienne Laurin <atnnn@rethinkdb.com>
2014-03-12 15:23:17 -0700

    get things to compile again

--- src/rdb_protocol/terms/error.cc
+++ src/rdb_protocol/terms/error.cc
@@ -22,55 +22,17 @@ private:
     virtual const char *name() const { return "error"; }
 };
 
-class default_term_t : public grouped_seq_op_term_t {
-public:
-    default_term_t(compile_env_t *env, const protob_t<const Term> &term)
-        : grouped_seq_op_term_t(env, term, argspec_t(2)) { }
-private:
-    virtual counted_t<val_t> eval_impl(scope_env_t *env, UNUSED eval_flags_t flags) {
-        return new_val(env->env, arg(env, 0)->as_seq(env->env)->add_transformation(
-            env->env, default_wire_func_t(arg(env, 1)->as_func()), backtrace()));
-    }
-    virtual const char *name() const { return "map"; }
-};
-
 class default_term_t : public op_term_t {
 public:
     default_term_t(compile_env_t *env, const protob_t<const Term> &term)
         : op_term_t(env, term, argspec_t(2)) { }
 private:
-    virtual counted_t<val_t> eval_impl(scope_env_t *env, UNUSED eval_flags_t flags) {
-        counted_t<const datum_t> func_arg;
-        scoped_ptr_t<exc_t> err;
-        counted_t<val_t> v;
-        try {
-            v = arg(env, 0);
-            if (v->get_type().is_convertible(val_t::type_t::DATUM)) {
-                func_arg = v->as_datum();
-                if (func_arg->get_type() != datum_t::R_NULL) {
-                    return v;
-                }
-            } else {
-                return v;
-            }
-        } catch (const exc_t &e) {
-            if (e.get_type() == base_exc_t::NON_EXISTENCE) {
-                err.init(new exc_t(e));
-                func_arg = make_counted<const datum_t>(e.what());
-            } else {
-                throw;
-            }
-        } catch (const datum_exc_t &e) {
-            if (e.get_type() == base_exc_t::NON_EXISTENCE) {
-                err.init(new exc_t(e.get_type(), e.what(), backtrace().get()));
-                func_arg = make_counted<const datum_t>(e.what());
-            } else {
-                throw;
-            }
-        }
+    counted_t<val_t>
+    default_value(counted_t<const datum_t> &&func_arg, scope_env_t *env, const exc_t *err) {
         r_sanity_check(func_arg.has());
         r_sanity_check(func_arg->get_type() == datum_t::R_NULL
                        || func_arg->get_type() == datum_t::R_STR);
+
         try {
             counted_t<val_t> def = arg(env, 1);
             if (def->get_type().is_convertible(val_t::type_t::FUNC)) {
@@ -80,15 +42,75 @@ private:
             }
         } catch (const base_exc_t &e) {
             if (e.get_type() == base_exc_t::EMPTY_USER) {
-                if (err.has()) {
+                if (err != NULL) {
                     throw *err;
                 } else {
+                    // TODO this behaviour seems unintuitive to me
+                    //      null.default(r.error()) -> null
                     r_sanity_check(func_arg->get_type() == datum_t::R_NULL);
-                    return v;
+                    return make_counted<val_t>(func_arg, backtrace());
                 }
             } else {
                 throw;
             }
+        }
+
+    }
+
+    counted_t<val_t>
+    handle_non_existence(counted_t<const datum_t> &&d, scope_env_t *env) {
+        if (d->get_type() != datum_t::R_NULL) {
+            return make_counted<val_t>(d, backtrace());
+        }
+        return default_value(std::move(d), env, NULL);
+    }
+
+    counted_t<val_t> handle_non_existence(const exc_t &e, scope_env_t *env) {
+        if (e.get_type() == base_exc_t::NON_EXISTENCE) {
+            return default_value(make_counted<const datum_t>(e.what()), env, &e);
+        }
+        throw e;
+    }
+
+    counted_t<val_t>
+    handle_non_existence(exc_wrapper_t<counted_t<const datum_t> > &&ew, scope_env_t *env) {
+        if (ew.has_exc()) {
+            return handle_non_existence(ew.get_exc(), env);
+        } else {
+            return handle_non_existence(std::move(*ew), env);
+        }
+    }
+
+    virtual counted_t<val_t> eval_impl(scope_env_t *env, UNUSED eval_flags_t flags) {
+        counted_t<val_t> v;
+        counted_t<const datum_t> d;
+        counted_t<grouped_data_t> gd;
+        try {
+            v = arg(env, 0);
+
+            gd = v->maybe_as_promiscuous_grouped_data(env->env);
+
+            if (!gd.has() && v->get_type().is_convertible(val_t::type_t::DATUM)) {
+                d = v->as_datum();
+            }
+
+        } catch (const exc_t &e) {
+            return handle_non_existence(e, env);
+
+        } catch (const datum_exc_t &e) {
+            exc_t err(e.get_type(), e.what(), backtrace().get());
+            return handle_non_existence(err, env);
+        }
+
+        if (gd.has()) {
+            for (auto kv = gd->begin(); kv != gd->end(); ++kv) {
+                kv->second = handle_non_existence(std::move(kv->second), env)->as_datum();
+            }
+            return make_counted<val_t>(gd, backtrace());
+        } else if (d.has()) {
+            return handle_non_existence(v->as_datum(), env);
+        } else {
+            return v;
         }
     }
     virtual const char *name() const { return "default"; }
