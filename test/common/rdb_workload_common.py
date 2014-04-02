712a63d44614450c8dccbc134175df70305ae235
Etienne Laurin <atnnn@rethinkdb.com>
2014-04-02 14:44:27 -0700

    Made regression-659 test pass

--- test/common/rdb_workload_common.py
+++ test/common/rdb_workload_common.py
@@ -1,6 +1,7 @@
 from vcoptparse import *
 import contextlib
 import rethinkdb as r
+import http_admin
 
 def option_parser_for_connect():
     op = OptParser()
@@ -14,4 +15,15 @@ def make_table_and_connection(opts):
     with r.connect(host, int(port)) as conn:
         (db, table) = opts['table'].split('.')
         yield (r.db(db).table(table), conn)
-    
+
+def insert_many(host="localhost", port=28015, db="test", table=None, count=10000):
+    conn = r.connect(host, port)
+    batch_size = 1000
+
+    def gen(i):
+        return { 'val': "X" * (i % 100) }
+
+    for start in range(0, count, batch_size):
+        end = min(start + batch_size, count)
+        res = r.db(db).table(table).insert([gen(i) for i in range(start, end)]).run(conn)
+        assert res['inserted'] == end - start
