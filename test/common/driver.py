c9589f9995bc89be7a8436ca6463228b2c10da88
Karl Kuehn <karl@rethinkdb.com>
2014-07-02 12:33:45 -0700

    combined python 3 changes

--- test/common/driver.py
+++ test/common/driver.py
@@ -53,7 +53,7 @@ def cleanupMetaclusterFolder(path):
     if os.path.isdir(str(path)):
         try:
             shutil.rmtree(path)
-        except Exception, e:
+        except Exception as e:
             print('Warning: unable to cleanup Metacluster folder: %s - got error: %s' % (str(path), str(e)))
 
 def get_table_host(processes):
@@ -215,7 +215,8 @@ class Files(object):
         else:
             self.machine_name = machine_name
 
-        create_args = command_prefix + [executable_path, "create",
+        create_args = command_prefix + [
+            executable_path, "create",
             "--directory", self.db_path,
             "--machine-name", self.machine_name]
 
@@ -280,7 +281,7 @@ class _Process(object):
 
             self.read_ports_from_log()
 
-        except Exception, e:
+        except Exception:
             # `close()` won't be called because we haven't put ourself into
             #  `cluster.processes` yet, so we have to clean up manually
             for other_cluster in cluster.metacluster.clusters:
@@ -299,7 +300,7 @@ class _Process(object):
             s = socket.socket()
             try:
                 s.connect(("localhost", self.http_port))
-            except socket.error, e:
+            except socket.error:
                 time.sleep(1)
             else:
                 break
@@ -314,9 +315,9 @@ class _Process(object):
             self.check()
             try:
                 log = open(self.logfile_path, 'r').read()
-                cluster_ports = re.findall("(?<=Listening for intracluster connections on port )([0-9]+)",log)
-                http_ports = re.findall("(?<=Listening for administrative HTTP connections on port )([0-9]+)",log)
-                driver_ports = re.findall("(?<=Listening for client driver connections on port )([0-9]+)",log)
+                cluster_ports = re.findall("(?<=Listening for intracluster connections on port )([0-9]+)", log)
+                http_ports = re.findall("(?<=Listening for administrative HTTP connections on port )([0-9]+)", log)
+                driver_ports = re.findall("(?<=Listening for client driver connections on port )([0-9]+)", log)
                 if cluster_ports == [] or http_ports == []:
                     time.sleep(1)
                 else:
@@ -324,7 +325,7 @@ class _Process(object):
                     self.http_port = int(http_ports[-1])
                     self.driver_port = int(driver_ports[-1])
                     break
-            except IOError, e:
+            except IOError:
                 time.sleep(1)
 
         else:
@@ -407,16 +408,15 @@ class Process(_Process):
         self.local_cluster_port = 29015 + self.port_offset
 
         options = ["serve",
-                   "--directory",  self.files.db_path,
-                   "--port-offset",  str(self.port_offset),
-                   "--client-port",  str(self.local_cluster_port),
+                   "--directory", self.files.db_path,
+                   "--port-offset", str(self.port_offset),
+                   "--client-port", str(self.local_cluster_port),
                    "--cluster-port", "0",
                    "--driver-port", "0",
                    "--http-port", "0"
                    ] + extra_options
 
-        _Process.__init__(self, cluster, options,
-            log_path=log_path, executable_path=executable_path, command_prefix=command_prefix)
+        _Process.__init__(self, cluster, options, log_path=log_path, executable_path=executable_path, command_prefix=command_prefix)
 
 class ProxyProcess(_Process):
     """A `ProxyProcess` object represents a running RethinkDB proxy. It cannot be
@@ -441,13 +441,12 @@ class ProxyProcess(_Process):
         self.local_cluster_port = 28015 + self.port_offset
 
         options = ["proxy",
-                   "--log-file",  self.logfile_path,
-                   "--port-offset",  str(self.port_offset),
-                   "--client-port",  str(self.local_cluster_port)
+                   "--log-file", self.logfile_path,
+                   "--port-offset", str(self.port_offset),
+                   "--client-port", str(self.local_cluster_port)
                    ] + extra_options
 
-        _Process.__init__(self, cluster, options,
-            log_path=log_path, executable_path=executable_path, command_prefix=command_prefix)
+        _Process.__init__(self, cluster, options, log_path=log_path, executable_path=executable_path, command_prefix=command_prefix)
 
 if __name__ == "__main__":
     with Metacluster() as mc:
@@ -456,4 +455,3 @@ if __name__ == "__main__":
         p = Process(c, f)
         time.sleep(3)
         p.check_and_stop()
-
