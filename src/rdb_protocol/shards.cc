12e74477c4f16e4a53beb9fbda66d3940094c0a6
Sam Hughes <sam@rethinkdb.com>
2014-07-02 13:34:04 -0700

    Removed an RSI comment.

--- src/rdb_protocol/shards.cc
+++ src/rdb_protocol/shards.cc
@@ -594,7 +594,6 @@ scoped_ptr_t<eager_acc_t> make_eager_terminal(const terminal_variant_t &t) {
 class ungrouped_op_t : public op_t {
 protected:
 private:
-    // RSI: What the fuck is a groups_t?  A std::map?  Why is sindex_val ignored?
     virtual void apply_op(env_t *env, groups_t *groups,
                           UNUSED const counted_t<const datum_t> &sindex_val) const {
         for (auto it = groups->begin(); it != groups->end();) {
