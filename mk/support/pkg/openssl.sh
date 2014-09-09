90cdf5d6b2f70cb1c040c90b9f3460f99259d69b
Etienne Laurin <atnnn@rethinkdb.com>
2014-09-08 19:21:50 -0700

    stash

--- mk/support/pkg/openssl.sh
+++ mk/support/pkg/openssl.sh
@@ -5,18 +5,16 @@ src_url="https://www.openssl.org/source/openssl-$version.tar.gz"
 
 
 pkg_configure () {
-    in_dir "$build_dir" ./config --prefix="$(niceabspath "$install_dir")"
-}
-
-pkg_install-include () {
-    pkg_copy_src_to_build
-    pkg_configure
-    pkg_make 
+    in_dir "$build_dir" ./config no-shared --prefix="$(niceabspath "$install_dir")"
 }
 
 pkg_install () {
     pkg_copy_src_to_build
+
     pkg_configure
-    pkg_make
+
+    # Compiling without -j1 causes a lot of "undefined reference" errors
+    pkg_make -j1
+
     pkg_make install
 }
