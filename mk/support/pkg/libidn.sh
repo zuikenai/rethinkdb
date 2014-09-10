a10489937fb19dd8be0e4418af2f2e1bfd606911
Etienne Laurin <atnnn@rethinkdb.com>
2014-09-09 18:33:54 -0700

    Tweaks for building v1.14.1

--- mk/support/pkg/libidn.sh
+++ mk/support/pkg/libidn.sh
@@ -2,3 +2,12 @@
 version=1.28
 
 src_url=http://ftp.gnu.org/gnu/libidn/libidn-$version.tar.gz
+
+pkg_link-flags () {
+    local flags
+    flags=$install_dir/lib/libidn.a
+    if [[ "$OS" = "Darwin" ]]; then
+        flags="$flags -liconv"
+    fi
+    echo "$flags"
+}
