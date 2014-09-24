8c606561e273a1eb3341e020ef603347a7015ab0
Sam Hughes <sam@rethinkdb.com>
2014-09-24 13:48:23 -0700

    Deduped short_value_sizer_t type in the leaf node tests.

--- src/unittest/new_leaf_node_test.cc
+++ src/unittest/new_leaf_node_test.cc
@@ -6,36 +6,11 @@
 #include "btree/node.hpp"
 #include "serializer/buf_ptr.hpp"
 #include "unittest/gtest.hpp"
+#include "unittest/leaf_node_test.hpp"
 
 namespace unittest {
 
 namespace new_leaf_node_test {
-
-// RSI: This is a duplicate of old_leaf_node_test::short_value_sizer_t.
-class short_value_sizer_t : public value_sizer_t {
-public:
-    explicit short_value_sizer_t(default_block_size_t bs) : block_size_(bs) { }
-
-    int size(const void *value) const {
-        int x = *reinterpret_cast<const uint8_t *>(value);
-        return 1 + x;
-    }
-
-    bool fits(const void *value, int length_available) const {
-        return length_available > 0 && size(value) <= length_available;
-    }
-
-    int max_possible_size() const {
-        return 256;
-    }
-
-    default_block_size_t default_block_size() const { return block_size_; }
-
-private:
-    default_block_size_t block_size_;
-
-    DISABLE_COPYING(short_value_sizer_t);
-};
 
 using orig_leaf_t = new_leaf_t<orig_btree_t>;
 
