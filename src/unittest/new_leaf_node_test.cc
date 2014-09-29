3cfbd9a066fe81e2784e29fad54ebc00d6571c41
Sam Hughes <sam@rethinkdb.com>
2014-09-29 14:27:39 -0700

    Made code call standard_serializer_t::create, made NewLeafNodeTest.InsertFind pass in partly-written state.

--- src/unittest/new_leaf_node_test.cc
+++ src/unittest/new_leaf_node_test.cc
@@ -12,6 +12,7 @@
 #include "unittest/gtest.hpp"
 #include "unittest/leaf_node_test.hpp"
 #include "unittest/mock_file.hpp"
+#include "unittest/unittest_utils.hpp"
 
 namespace unittest {
 
@@ -19,31 +20,41 @@ namespace new_leaf_node_test {
 
 using main_leaf_t = new_leaf_t<main_btree_t>;
 
-class test_cache_t
-    : private mock_file_opener_t,
-      private standard_serializer_t,
-      private dummy_cache_balancer_t,
-      public cache_t {
+scoped_ptr_t<standard_serializer_t> create_and_construct_serializer(serializer_file_opener_t *opener) {
+    standard_serializer_t::create(opener, standard_serializer_t::static_config_t());
+    return make_scoped<standard_serializer_t>(standard_serializer_t::dynamic_config_t(),
+                                              opener,
+                                              &get_global_perfmon_collection());
+}
+
+class test_cache_t {
 public:
-    using cache_t::default_block_size;
     test_cache_t()
-        : mock_file_opener_t(),
-          standard_serializer_t(standard_serializer_t::dynamic_config_t(),
-                                this,
-                                &get_global_perfmon_collection()),
-          dummy_cache_balancer_t(GIGABYTE),
-          cache_t(this, this, &get_global_perfmon_collection())
+        : opener_(),
+          serializer_(create_and_construct_serializer(&opener_)),
+          balancer_(GIGABYTE),
+          cache_(serializer_.get(), &balancer_, &get_global_perfmon_collection())
     { }
+
+protected:
+    cache_t *get_cache() { return &cache_; }
+
+private:
+    mock_file_opener_t opener_;
+    scoped_ptr_t<standard_serializer_t> serializer_;
+    dummy_cache_balancer_t balancer_;
+    cache_t cache_;
 };
 
 class test_txn_t : private test_cache_t,
                    private cache_conn_t,
                    public txn_t {
 public:
-    using test_cache_t::default_block_size;
+    using txn_t::cache;
+
     test_txn_t()
         : test_cache_t(),
-          cache_conn_t(this),
+          cache_conn_t(test_cache_t::get_cache()),
           txn_t(this, write_durability_t::SOFT, repli_timestamp_t::distant_past) { }
 };
 
@@ -55,12 +66,14 @@ TEST(NewLeafNodeTest, InitValidate) {
     main_leaf_t::validate(&sizer, buf.sized_cache_data<main_leaf_node_t>());
 }
 
-TEST(NewLeafNodeTest, InsertFind) {
+TPTEST(NewLeafNodeTest, InsertFind) {
     test_txn_t txn;
-    short_value_sizer_t sizer(txn.default_block_size());
+    short_value_sizer_t sizer(txn.cache()->default_block_size());
     buf_lock_t lock(buf_parent_t(&txn), alt_create_t::create);
-
     buf_write_t write(&lock);
+    write.set_data_write(main_leaf_t::init());
+
+    
 
 
 }
