7cc5c0ae1596462a8c0e395c5d6c38ff06c2dba1
Karl Kuehn <karl@rethinkdb.com>
2014-10-09 10:58:09 -0700

    changes from code review

--- drivers/python/rethinkdb/net.py
+++ drivers/python/rethinkdb/net.py
@@ -151,15 +151,18 @@ class Connection(object):
             self.socket = socket.create_connection((self.host, self.port), self.timeout)
         except Exception as err:
             raise RqlDriverError("Could not connect to %s:%s. Error: %s" % (self.host, self.port, err))
-
-        self._sock_sendall(
-            struct.pack(
-                "<2L", p.VersionDummy.Version.V0_3, len(self.auth_key)
-            ) + self.auth_key +
-            struct.pack("<L", p.VersionDummy.Protocol.JSON))
-
-        # Read out the response from the server, which will be a null-terminated string
+        
         try:
+            # Send our inial handshake
+            
+            self._sock_sendall(
+                struct.pack("<2L", p.VersionDummy.Version.V0_3), len(self.auth_key)) +
+                self.auth_key +
+                struct.pack("<L", p.VersionDummy.Protocol.JSON)
+            )
+            
+            # Read out the response from the server, which will be a null-terminated string
+        
             response = b""
             while True:
                 char = self._sock_recv(1)
@@ -168,6 +171,8 @@ class Connection(object):
                 response += char
         except socket.timeout:
             raise RqlDriverError("Timed out during handshake with %s:%d." % (self.host, self.port))
+        except socket.error as err:
+            raise RqlDriverError("Error during handshake with %s:%d - %s" % (self.host, self.port, err))
 
         if response != b"SUCCESS":
             self.close(noreply_wait=False)
