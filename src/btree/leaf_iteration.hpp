d067f985953a9ba7cbf556fc0d54a0e263cdbf65
Sam Hughes <sam@rethinkdb.com>
2014-10-24 18:03:17 -0700

    leaf_iteration.hpp.

---
+++ src/btree/leaf_iteration.hpp
@@ -0,0 +1,17 @@
+// Copyright 2010-2014 RethinkDB, all rights reserved.
+#ifndef BTREE_LEAF_ITERATION_HPP_
+#define BTREE_LEAF_ITERATION_HPP_
+
+namespace leaf {
+
+// We define templated iteration methods here instead of having some leaf::iterator
+// type that repeats the is-old/is-new check every time.
+
+
+
+
+
+
+}  // namespace leaf
+
+#endif  // BTREE_LEAF_ITERATION_HPP_
